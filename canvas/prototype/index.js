9ab8ed6885f090ba6a2888ded5997e1a661a11ac
zhangsucheng <591016519@qq.com>
2019-11-29 11:03:11 +0800

    qq

---
+++ canvas/prototype/index.js
@@ -0,0 +1,15 @@
+let les  = {}
+let Animal = function  () {
+    console.log("I AM ANIMAL")
+}
+Animal.prototype.eat = function () {
+    console.log("animal like eat banner")
+}
+les.animal = Animal
+export default les
+
+
+
+
+
+
--- canvas/src/test/canvas.vue
+++ canvas/src/test/canvas.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>123</div>
+    <div>
+        <div>1234</div>
+
+    </div>
+
 </template>
 
 <script>
+     import les from "suziyoudian-test"
     export default {
-        name: "canvas"
+        name: "canvas",
+        data(){
+            return {
+
+            }
+        },
+        components:{
+
+        },
+        mounted() {
+            console.log(123)
+            console.log(les)
+        }
     }
 </script>
 
