2a954d3a17fffb27dbf5b5124996e47b2e1ddce3
zhangsucheng <591016519@qq.com>
2020-02-24 19:33:50 +0800

    css3

---
+++ css3/src/components/background.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrap">
+        <div id="test-div">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "background"
+    }
+</script>
+
+<style scoped>
+    .wrap div{
+        width: 300px;
+        height: 120px;
+        margin-bottom: 30px;
+        border:15px solid transparent;
+    }
+    #test-div{
+
+    }
+</style>
--- css3/src/components/border.vue
+++ css3/src/components/border.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>123</div>
+    <div class="wrap">
+        <div id="border-round">
+            图像平铺（重复）来填充该区域。
+        </div>
+
+        <div id="border-stretch">
+            图像被拉伸以填充该区域
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +17,21 @@
 </script>
 
 <style scoped>
-
+    .wrap div{
+        width: 300px;
+        height: 120px;
+        margin-bottom: 30px;
+        border:15px solid transparent;
+    }
+    #border-round{
+        /*border-image: url("../assets/border.png") 30 30 round;*/
+        border-image-source: url("../assets/border.png");
+        border-image-slice: 30;
+        border-image-width: 15px;
+        /*border-image-outset:0;*/
+        border-image-repeat: round;
+    }
+    #border-stretch{
+        border-image: url("../assets/border.png") 30 30 stretch;
+    }
 </style>
