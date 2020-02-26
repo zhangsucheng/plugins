5158c3864658ff56fec7d9efdb02183c00cee253
zhangsucheng <591016519@qq.com>
2020-02-26 20:32:37 +0800

    transition

---
+++ css3/src/components/threed.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "threed"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ css3/src/components/twod.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "2D"
+    }
+</script>
+
+<style scoped>
+
+</style>
