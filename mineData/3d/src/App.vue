49596fb794c62478eb70b2b90d5dc21627e4d745
zhangsucheng <591016519@qq.com>
2021-01-25 17:30:52 +0800

    mineData3d

---
+++ mineData/3d/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <mineMap></mineMap>
+  </div>
+</template>
+
+<script>
+import mineMap from './components/mineMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    mineMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mineData/3d/src/components/mineMap.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="map-wrap" >
+    <h1>3D地图</h1>
+    <div id="map" style="width: 80vw;height: 80vh;border:1px solid #90EE90"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mineMap',
+  props: {
+
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
